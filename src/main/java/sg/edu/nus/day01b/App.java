80da3b60208b1bc029381b6546a3c95919658d1c
chengjia21 <ccj.pepe@protonmail.com>
2023-04-17 16:42:22 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/day01b/App.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.day01b;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
